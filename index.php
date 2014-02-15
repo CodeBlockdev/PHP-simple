4ae055f6e656a18cf9c708cf543c685bd77d71bd
MRC <matheuschimelli@live.com>
2014-02-14 23:48:28 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//vamos começar a criar uma função para a nossa primeira página php
+
+//usamos function para dizer que quremos criar uma função 
+
+//o myFunction no caso, seria o nome da sua função
+
+// o () pode ser usado para colocar variaveis do tipo $minhaVariavel dentro da função para que faça algo
+
+function myFunction($nome){
+
+// vamos dizer olá e o nome de uma pesoa
+
+print "Hello $nome";
+
+}
+
+para fazer a função funcionar dizemos myFuntion("nome");
+?>
